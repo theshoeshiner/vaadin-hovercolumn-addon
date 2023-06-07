588a25fd9c50753842939f346b5a8d52e966e398
daniel.watson <daniel.watson@bioclinica.com>
2023-06-07 16:41:26 -0400

    addon folder

---
+++ addon/src/main/java/org/thshsh/vaadin/grid/HoverColumn.java
@@ -0,0 +1,22 @@
+package org.thshsh.vaadin.grid;
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.dependency.CssImport;
+
+@SuppressWarnings("serial")
+@CssImport(value="show-on-hover-column.css")
+@CssImport(value = "show-on-hover-column-vaadin-grid.css",themeFor = "vaadin-grid")
+public class HoverColumn extends Component {
+
+	/**
+	 * Applying this class to a column via com.vaadin.flow.component.grid.Grid.Column.setClassNameGenerator() will hide the contents 
+	 * of the column unless the row is being hovered over
+	 */
+	public static final String HOVER_COLUMN_CLASS = "show-on-hover";
+	
+	/**
+	 * Applying this class to a component will force the component to be visible even if it is within a hidden column
+	 */
+	public static final String HOVER_COLUMN_EXCLUDE_CLASS = "show-on-hover-exclude";
+
+}
---
+++ addon/src/test/java/org/vaadin/example/addon/AddonView.java
@@ -0,0 +1,12 @@
+package org.vaadin.example.addon;
+
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class AddonView extends Div {
+
+    public AddonView() {
+        //TODO
+    }
+}
